a7b42057f1705fe1e938cc5bd20b9fc36d88486b
Angelo Fernandes Oliveira <00550162@ufrgs.br>
2025-07-15 20:24:56 -0300

    Update nodo.h

--- src/libs/nodo.h
+++ src/libs/nodo.h
@@ -4,56 +4,45 @@
 #include <string>
 #include <vector>
 #include <cstdint>
-#include <arpa/inet.h> // Para inet_addr
+#include <arpa/inet.h>
 
 #define BUFFER_SIZE 1024
 #define BROADCAST_ADDR "255.255.255.255"
 
-//tipo de mensagens trocadas
+// Tipos de mensagens trocadas
 enum class Type : uint8_t {
-    // Mensagens Cliente <-> Servidor
+    // Cliente <-> Servidor
     DESC,
     REQ,
     DESC_ACK,
     REQ_ACK,
     NOT_LEADER,
 
-    // Mensagens Servidor <-> Servidor (Descoberta e Replicação)
+    // Servidor <-> Servidor
     SERVER_DISCOVERY,
-    SERVER_DISCOVERY_ACK,
     REPLICATION_UPDATE,
     REPLICATION_ACK,
-    LEADER_ACK,
-    LEADER_ANNOUNCEMENT,
-    BACKUP_ACK,
-
-    // Mensagens de Eleição (Algoritmo Bully)
-    HEARTBEAT,          // Líder prova que está vivo
-    ELECTION,           // Um servidor inicia uma eleição
-    OK_ANSWER,          // Resposta a uma mensagem de eleição
-    COORDINATOR,         // Anúncio do novo líder
-
-    I_AM_ALIVE,
-    ARE_YOU_ALIVE,
+    HEARTBEAT,
+    // Mensagens como COORDINATOR, ELECTION, OK_ANSWER não são mais necessárias
+    // na arquitetura simplificada, mas podem ser mantidas se desejar.
 };
 
 #pragma pack(push, 1)
 struct Message {
     Type type;
-    uint32_t num;
     uint32_t seq;
-    uint32_t ip_addr; 
-
-    uint64_t total_sum;     
-    uint32_t total_reqs;     
-
+    uint32_t num;
     
-    uint64_t total_sum_server; 
-    uint32_t total_reqs_server; 
+    // Campos para diferentes propósitos
+    uint32_t ip_addr; 
+    uint64_t total_sum;
+    uint32_t total_reqs;
+    uint64_t total_sum_server;
+    uint32_t total_reqs_server;
 };
 #pragma pack(pop)
 
-
+// Estruturas de dados
 struct tableClient {
     std::string address;
     uint32_t last_req;
@@ -70,23 +59,22 @@ struct ServerInfo {
     std::string ip_address;
 };
 
-
+// Classe base
 class Nodo {
-    
-    public:
-        Nodo();
-        virtual ~Nodo();
+public:
+    Nodo();
+    virtual ~Nodo();
 
-        virtual std::string getHostname();
-        virtual std::string getIP();
-        int createSocket(int port);
-        void setSocketBroadcastOptions(int sockfd);
-        void setSocketTimeout(int sockfd, int timeoutSec);
+    virtual std::string getHostname();
+    virtual std::string getIP();
+    int createSocket(int port);
+    void setSocketBroadcastOptions(int sockfd);
+    void setSocketTimeout(int sockfd, int timeoutSec);
 
-    protected:
-        std::vector<tableClient> participants;
-        std::vector<ServerInfo> server_list;
-        tableAgregation sumTotal = {0, 0};
+protected:
+    std::vector<tableClient> participants;
+    std::vector<ServerInfo> server_list;
+    tableAgregation sumTotal = {0, 0};
 };
 
 #endif // NODO_H
