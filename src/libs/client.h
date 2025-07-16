2ac3629a729757b87933386403547462504d7464
Angelo Fernandes Oliveira <00550162@ufrgs.br>
2025-07-15 21:39:38 -0300

    Update client.h

--- src/libs/client.h
+++ src/libs/client.h
@@ -13,6 +13,7 @@ public:
     ~Client();
     
     std::string discoverServer(int Discovery_port, int Request_Port); 
+    bool sendOneRequest(uint32_t num, std::string& currentServerIP, int requestPort);
     bool sendNum(const std::string& serverIP, int Request_Port);
 
 private:
